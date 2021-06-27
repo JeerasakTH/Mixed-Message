7558d139121259387f70cace66832f5fcdb4d27f
Jeerasak Todsungnern <78092831+JeerasakTH@users.noreply.github.com>
2021-06-27 21:51:39 +0700

    minor fix

--- mixed-massage.js
+++ mixed-massage.js
@@ -29,7 +29,7 @@ const spinSlot = () => {
 
 const check = input => {
     console.log(input)
-    if (input[0] === input[1] || input[0] === input[2]) {
+    if (input[0] === input[1] || input[0] === input[2] || input[1] === input[2]) {
         console.log('You win')
     } else if (input[0] === input[1] === input[2]) {
         console.log('Jackpot Big prize!')
