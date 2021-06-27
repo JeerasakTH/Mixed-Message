e612c4209c7723763cb3c3d7c2a56333cc59d22e
Sivavet Thammahon <sivavet.job@gmail.com>
2021-06-27 23:58:04 +0700

    ทำให้เป็นออบเจค เพิ่มระบบเหรียญ

--- mixed-massage.js
+++ mixed-massage.js
@@ -1,42 +1,39 @@
-const input = ['Sherry', 'Bell', 'Coin', '7']
+const slot = {
+    coin: 0,
+    _spin:['Sherry', 'Bell', 'Coin', '7'],
+    _result: [],
 
+    randomSlot(random){
+       return random === 0 ? this._result.push(this._spin[0]): 
+       random === 1 ? this._result.push(this._spin[1]):
+       random === 2 ? this._result.push(this._spin[2]):
+       this._result.push(this._spin[3])
+    },
 
-const randomSlot = () => {
-    const random = Math.floor(Math.random() * input.length)
-    switch (random) {
-        case 0:
-            return input[0];
-            break;
-        case 1:
-            return input[1];
-            break;
-        case 2:
-            return input[2];
-            break;
-        case 3:
-            return input[3];
-            break;
+    get check(){
+        const chat = '';
+        if (this._result[0] === this._result[1] || this._result[0] === this._result[2] || this._result[1] === this._result[2]) {
+            this.chat = 'You win';
+            this.coin += 2;
+        } else if (this._result[0] === this._result[1] === this._result[2]) {
+            this.chat = 'Jackpot Big prize!';
+            this.coin += this.coin;
+        } else {
+            this.chat = 'You\'ve lose all of your coin';
+            this.coin -= this.coin;
+        }
+        // console.log(this.coin)
+        console.log(`${this._result.join(' ')}\n${this.chat} and you have ${this.coin} left`)
     }
+
 }
 
-const spinSlot = () => {
-    const result = [];
-    result.push(randomSlot());
-    result.push(randomSlot());
-    result.push(randomSlot());
-    return result;
+
+for (let i=3; i>0; i--){
+    random = Math.floor(Math.random() * 4)
+    slot.randomSlot(random)
 }
 
-const check = input => {
-    console.log(input)
-    if (input[0] === input[1] || input[0] === input[2] || input[1] === input[2]) {
-        console.log('You win')
-    } else if (input[0] === input[1] === input[2]) {
-        console.log('Jackpot Big prize!')
-    } else {
-        console.log('Try again')
-    }
-    return input
-}
+slot.check
 
-check(spinSlot());
+
